552e4d2eeebd769cca3a176ced44e047ca44b6aa
Alex Vazquez <alex.quez@gmail.com>
2024-11-05 14:52:29 -0800

    Add direct support for ruby 3.0

--- lib/stripe_mock/api/instance.rb
+++ lib/stripe_mock/api/instance.rb
@@ -1,18 +1,28 @@
 module StripeMock
-
   @state = 'ready'
   @instance = nil
   @original_execute_request_method = Stripe::StripeClient.instance_method(:execute_request)
 
   def self.start
     return false if @state == 'live'
+
     @instance = instance = Instance.new
-    Stripe::StripeClient.send(:define_method, :execute_request) { |*args, **keyword_args| instance.mock_request(*args, **keyword_args) }
+    # Monkey patch Stripe::StripeClient#execute_request to support ruby 3.0 kwargs
+    Stripe::StripeClient.send(:define_method, :execute_request) do |*args, **kwargs|
+      # Check if kwargs is empty and call mock_request accordingly
+      if kwargs.empty?
+        instance.mock_request(*args)
+      else
+        instance.mock_request(*args, **kwargs)
+      end
+    end
+
     @state = 'local'
   end
 
   def self.stop
     return unless @state == 'local'
+
     restore_stripe_execute_request_method
     @instance = nil
     @state = 'ready'
