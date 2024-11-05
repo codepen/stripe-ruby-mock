6cfb54e9ad124871cde9ddfd3ec30451ef88b932
Alex Vazquez <alex.quez@gmail.com>
2024-11-05 15:07:31 -0800

    Mock raw request

--- lib/stripe_mock/api/instance.rb
+++ lib/stripe_mock/api/instance.rb
@@ -1,28 +1,18 @@
 module StripeMock
+
   @state = 'ready'
   @instance = nil
-  @original_execute_request_method = Stripe::StripeClient.instance_method(:execute_request)
+  @original_execute_request_method = Stripe::StripeClient.instance_method(:raw_request)
 
   def self.start
     return false if @state == 'live'
-
     @instance = instance = Instance.new
-    # Monkey patch Stripe::StripeClient#execute_request to support ruby 3.0 kwargs
-    Stripe::StripeClient.send(:define_method, :execute_request) do |*args, **kwargs|
-      # Check if kwargs is empty and call mock_request accordingly
-      if kwargs.empty?
-        instance.mock_request(*args)
-      else
-        instance.mock_request(*args, **kwargs)
-      end
-    end
-
+    Stripe::StripeClient.send(:define_method, :raw_request) { |*args, **keyword_args| instance.mock_request(*args, **keyword_args) }
     @state = 'local'
   end
 
   def self.stop
     return unless @state == 'local'
-
     restore_stripe_execute_request_method
     @instance = nil
     @state = 'ready'
@@ -39,7 +29,7 @@ module StripeMock
   end
 
   def self.restore_stripe_execute_request_method
-    Stripe::StripeClient.send(:define_method, :execute_request, @original_execute_request_method)
+    Stripe::StripeClient.send(:define_method, :raw_request, @original_execute_request_method)
   end
 
   def self.instance; @instance; end
